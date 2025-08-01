6180a1c0c8f4f391e10746c8ae4d42bfa45b48fe
Lucas Fernandes <lucasafernandes94@gmail.com>
2025-08-01 13:40:43 -0300

    error in probe - 1.0.1

--- app/main.py
+++ app/main.py
@@ -4,7 +4,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello from GitOps App! Argo CD Rollback - 1.0.0"
-
-@app.route("/actuator/health")
-def health():
-    return { "status": "UP" }
