3e3cfc47807606971617b6d3c4c35ae4a15d685c
Lucas Fernandes <lucasafernandes94@gmail.com>
2025-08-01 13:37:30 -0300

    starting test of rollback - 1.0.0

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitOps App! Argo CD Rollback"
+    return "Hello from GitOps App! Argo CD Rollback - 1.0.0"
 
 @app.route("/actuator/health")
 def health():
