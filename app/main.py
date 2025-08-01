e64b2d434ac241df08260e10fa6b8fa6dddd36e1
Lucas Fernandes <lucasafernandes94@gmail.com>
2025-08-01 14:35:33 -0300

    new version - 1.0.5

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.roudte("/")
+@app.route("/")
 def home():
-    return "Hello from GitOps App! Argo CD Rollback - 1.0.4"
+    return "Hello from GitOps App! Argo CD Rollback - 1.0.5"
+
+@app.route("/actuator/health")
+def health():
+    return { "status": "UP" }
